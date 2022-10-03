9c53984090a410af51caf351a807c6811b3401a2
Gilles Nikles <gilles.nikles@gmail.com>
2022-10-03 22:28:10 +0200

    frontend setup and navbar

---
+++ frontend_react/src/container/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { images } from "../../constants";
+import "./Header.scss";
+
+const Header = () => {
+  return <div>Header</div>;
+};
+
+export default Header;
