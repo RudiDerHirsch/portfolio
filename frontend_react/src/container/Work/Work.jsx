860c65a73a096de5b68be500c41b3bc0a4834f28
Gilles Nikles <gilles.nikles@gmail.com>
2022-10-26 14:34:19 +0200

    work and skills separated

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,10 +1,45 @@
 import React from "react";
-
-import { images } from "../../constants";
-import "./Work.scss";
+import { Chrono } from "react-chrono";
 
 const Work = () => {
-  return <div>Work</div>;
+  const items = [
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "http://someurl/image.jpg",
+        },
+      },
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "http://someurl/image.jpg",
+        },
+      },
+    },
+  ];
+
+  return (
+    <div style={{ width: "500px", height: "400px" }}>
+      <Chrono items={items} />
+    </div>
+  );
 };
 
 export default Work;
